952764a8f245cdb7baa8393fb049c76c366fbe66
ISKL-wyz <1453375019@qq.com>
2021-07-15 11:15:16 +0800

    WY

--- TDGame/Assets/C#/Enemy/EnemySpawner.cs
+++ TDGame/Assets/C#/Enemy/EnemySpawner.cs
@@ -6,13 +6,24 @@ public class EnemySpawner : MonoBehaviour
 {
     public static int CountEnemyAlive = 0;
     public Wave[] waves;
+    public List<GameObject> listEnemy = new List<GameObject>();
     public Transform START;
     public float waveRate = 3;
-
+    private Coroutine coroutine;
 
     void Start()
     {
-        StartCoroutine(SpawnEnemy());
+        coroutine = StartCoroutine(SpawnEnemy());
+    }
+
+    public void Stop()
+    {
+        StopCoroutine(coroutine);
+    }
+
+    public void Remove(GameObject gameObject)
+    {
+        listEnemy.Remove(gameObject);
     }
 
     IEnumerator SpawnEnemy()
@@ -21,6 +32,7 @@ public class EnemySpawner : MonoBehaviour
         {
             for(int i = 0; i < wave.count; i++) {
                 GameObject.Instantiate(wave.enemyPrefab, START.position, Quaternion.identity);
+                listEnemy.Add(this.gameObject);
                 CountEnemyAlive++;
                 if(i!=wave.count-1)
                 yield return new WaitForSeconds(wave.rate);
@@ -32,5 +44,21 @@ public class EnemySpawner : MonoBehaviour
 
             yield return new WaitForSeconds(waveRate);
         }
+    }
+
+    private void UpdateEnemy()
+    {
+        for (int i = 0; i < listEnemy.Count; i++)
+        {
+            if (listEnemy[i] == null)
+            {
+                listEnemy[i] = listEnemy[i + 1];
+            }
+        }
+    }
+
+    void Update()
+    {
+        UpdateEnemy();
     }
 }
